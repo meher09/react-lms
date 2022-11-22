6732fd30d43edb4a9c9e1b12bcd99c369d15aa56
meher09 <mehernigarcu@gmail.com>
2022-11-22 23:23:49 +0600

    Initial Commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../Shared/Navigation';
+import Banner from './Banner';
+
+const Home = () => {
+    return (
+        <>
+            <Navigation></Navigation>
+            <Banner></Banner>
+        </>
+    );
+};
+
+export default Home;
